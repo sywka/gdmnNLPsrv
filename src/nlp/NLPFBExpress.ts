feddb1b80744514f46f102afae0d71c374377e00
sywka1 <lomashko.yuri@gmail.com>
2018-03-01 13:13:48 +0300

    update libs

--- src/nlp/NLPFBExpress.ts
+++ src/nlp/NLPFBExpress.ts
@@ -4,14 +4,12 @@ import NLPFBAdapter from "./adapter/fb/NLPFBAdapter";
 
 export default class NLPFBExpress extends FBExpress {
 
-    protected _createSchema(options: FBExpressOptions): Schema<FBGraphQLContext> {
-        const schema = new NLPSchema({
+    protected _getSchema(options: FBExpressOptions): Schema<FBGraphQLContext> {
+        return new NLPSchema({
             adapter: new NLPFBAdapter({
                 ...(options as DBOptions),
                 blobLinkCreator: this._createBlobUrl.bind(this),
             })
         });
-        schema.createSchema().catch(console.error);
-        return schema;
     }
 }
