d59d732e626c536fc91c592037725f63bf2b96f8
sywka1 <lomashko.yuri@gmail.com>
2018-03-02 15:38:48 +0300

    update graphql-sql-bridge

--- src/nlp/NLPFBExpress.ts
+++ src/nlp/NLPFBExpress.ts
@@ -1,4 +1,4 @@
-import {DBOptions, FBExpress, FBExpressOptions, FBGraphQLContext, Schema} from "graphql-sql-bridge";
+import {FBExpress, FBExpressOptions, FBGraphQLContext, ISchemaDetailOptions, Schema} from "graphql-sql-bridge";
 import NLPSchema from "./NLPSchema";
 import NLPFBAdapter from "./adapter/fb/NLPFBAdapter";
 
@@ -6,8 +6,8 @@ export default class NLPFBExpress extends FBExpress {
 
     protected _getSchema(options: FBExpressOptions): Schema<FBGraphQLContext> {
         return new NLPSchema({
-            adapter: new NLPFBAdapter({
-                ...(options as DBOptions),
+            adapter: new NLPFBAdapter(this.connectionPool, {
+                ...(<ISchemaDetailOptions>options),
                 blobLinkCreator: this._createBlobUrl.bind(this),
             })
         });
--- src/nlp/adapter/fb/NLPFBAdapter.ts
+++ src/nlp/adapter/fb/NLPFBAdapter.ts
@@ -1,5 +1,5 @@
 import NestHydrationJS from "nesthydrationjs";
-import {FBDatabase, FBAdapter} from "graphql-sql-bridge";
+import {DBOptions, FBAdapter, FBDatabase} from "graphql-sql-bridge";
 import {INLPField, INLPTable} from "../../NLPSchema";
 
 export interface IIndex {
@@ -22,59 +22,6 @@ export interface IFBField extends IBaseIndexing {
 
 export default class NLPFBAdapter extends FBAdapter {
 
-    protected static async _getNLPTables(database: FBDatabase): Promise<IFBTable[]> {
-        const result: any[] = await database.query(`
-          SELECT
-            TRIM(tables.usr$relation_name)                              AS "tableName",
-            TRIM(tables.usr$relation_name) 
-              || '_' || TRIM(fields.usr$field_name)                     AS "fieldKey",
-            
-            TRIM(REPLACE(entities.usr$name,  ',', ''))                  AS "tableIndexName",
-            TRIM(REPLACE(attr.usr$name,  ',', ''))                      AS "fieldIndexName",
-            ref_type.id                                                 AS "refKey",
-            ref_type.usr$description                                    AS "refDescription",
-            TRIM(REPLACE(ref_type_detail.usr$name,  ',', ''))           AS "refTypeIndexName"
-                
-          FROM usr$nlp_table tables
-          
-            LEFT JOIN usr$nlp_tentities entities
-              ON entities.usr$table_key = tables.id
-              AND TRIM(REPLACE(entities.usr$name,  ',', '')) > ''
-            LEFT JOIN usr$nlp_field fields
-              ON fields.usr$table_key = tables.id
-              LEFT JOIN usr$nlp_tentities_attr attr
-                ON attr.usr$field_key = fields.id
-                AND TRIM(REPLACE(attr.usr$name,  ',', '')) > ''
-              LEFT JOIN usr$nlp_field_ref field_ref ON field_ref.USR$FIELD_KEY = fields.ID
-                LEFT JOIN usr$nlp_ref_type ref_type ON ref_type.id = field_ref.usr$ref_type_key
-                  LEFT JOIN usr$nlp_ref_type_detail ref_type_detail
-                    ON ref_type_detail.usr$ref_type_key = ref_type.id
-                    AND TRIM(REPLACE(ref_type_detail.usr$name,  ',', '')) > ''
-        `);
-
-        const definition: any = [{
-            id: {column: "tableName", id: true},
-            indices: [{
-                key: "tableIndexName"
-            }],
-            fields: [{
-                id: {column: "fieldKey", id: true},
-                indices: [{
-                    key: "fieldIndexName"
-                }],
-                refs: [{
-                    id: {column: "refKey", type: "NUMBER"},
-                    name: "refDescription",
-                    indices: [{
-                        key: "refTypeIndexName"
-                    }]
-                }]
-            }]
-        }];
-
-        return NestHydrationJS().nest(result, definition);
-    }
-
     private static _createDescription(indices: IIndex[], name?: string): string {
         const description = name ? name : "";
         if (!indices) return description;
@@ -86,11 +33,64 @@ export default class NLPFBAdapter extends FBAdapter {
         }, description);
     }
 
-    protected async _queryToDatabase(database: FBDatabase): Promise<INLPTable[]> {
-        const tables = await super._queryToDatabase(database) as INLPTable[];
+
+    public async getTables(): Promise<INLPTable[]> {
+        const tables = await super.getTables();
 
         try {
-            const nlpTables = await NLPFBAdapter._getNLPTables(database);
+            const nlpTables = await FBDatabase.executeDatabase<IFBTable[]>(<DBOptions>this.source,
+                async (database: FBDatabase) => {
+                    const result: any[] = await database.query(`
+                      SELECT
+                        TRIM(tables.usr$relation_name)                              AS "tableName",
+                        TRIM(tables.usr$relation_name) 
+                          || '_' || TRIM(fields.usr$field_name)                     AS "fieldKey",
+                        
+                        TRIM(REPLACE(entities.usr$name,  ',', ''))                  AS "tableIndexName",
+                        TRIM(REPLACE(attr.usr$name,  ',', ''))                      AS "fieldIndexName",
+                        ref_type.id                                                 AS "refKey",
+                        ref_type.usr$description                                    AS "refDescription",
+                        TRIM(REPLACE(ref_type_detail.usr$name,  ',', ''))           AS "refTypeIndexName"
+                            
+                      FROM usr$nlp_table tables
+                      
+                        LEFT JOIN usr$nlp_tentities entities
+                          ON entities.usr$table_key = tables.id
+                          AND TRIM(REPLACE(entities.usr$name,  ',', '')) > ''
+                        LEFT JOIN usr$nlp_field fields
+                          ON fields.usr$table_key = tables.id
+                          LEFT JOIN usr$nlp_tentities_attr attr
+                            ON attr.usr$field_key = fields.id
+                            AND TRIM(REPLACE(attr.usr$name,  ',', '')) > ''
+                          LEFT JOIN usr$nlp_field_ref field_ref ON field_ref.USR$FIELD_KEY = fields.ID
+                            LEFT JOIN usr$nlp_ref_type ref_type ON ref_type.id = field_ref.usr$ref_type_key
+                              LEFT JOIN usr$nlp_ref_type_detail ref_type_detail
+                                ON ref_type_detail.usr$ref_type_key = ref_type.id
+                                AND TRIM(REPLACE(ref_type_detail.usr$name,  ',', '')) > ''
+                    `);
+
+                    const definition: any = [{
+                        id: {column: "tableName", id: true},
+                        indices: [{
+                            key: "tableIndexName"
+                        }],
+                        fields: [{
+                            id: {column: "fieldKey", id: true},
+                            indices: [{
+                                key: "fieldIndexName"
+                            }],
+                            refs: [{
+                                id: {column: "refKey", type: "NUMBER"},
+                                name: "refDescription",
+                                indices: [{
+                                    key: "refTypeIndexName"
+                                }]
+                            }]
+                        }]
+                    }];
+
+                    return NestHydrationJS().nest(result, definition);
+                });
 
             nlpTables.forEach((nlpTable) => {
                 const table = tables.find((table) => table.id === nlpTable.id);
@@ -110,7 +110,7 @@ export default class NLPFBAdapter extends FBAdapter {
                 }
             });
         } catch (error) {
-            console.warn(error);
+            console.error(error);
         }
 
         return tables;
